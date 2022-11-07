4c6ef84b4b63ba20767d63314eaaa279d987e2d6
JooMin Choi <joomin.choi@clearscore.com>
2022-11-07 10:06:32 +0000

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by JooMin Choi on 07/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
